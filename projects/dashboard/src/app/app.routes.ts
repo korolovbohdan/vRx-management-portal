9270013da548bdd016f8e983b6dfb274755d128e
BohdanKorolev <bohdan.korolev@gmail.com>
2025-11-24 21:49:43 +0200

    add initial project structure with multiple applications and routing configuration

---
+++ projects/dashboard/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./app').then(m => m.App)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import {Routes} from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/native-federation';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      loadRemoteModule('dashboard', './Routes').then((m) => m.routes),
+  },
+  {
+    path: 'assets',
+    loadChildren: () =>
+      loadRemoteModule('assets', './Routes').then((m) => m.routes),
+  },
+  {
+    path: 'findings',
+    loadChildren: () =>
+      loadRemoteModule('findings', './Routes').then((m) => m.routes),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      loadRemoteModule('users', './Routes').then((m) => m.routes),
+  },
+];
